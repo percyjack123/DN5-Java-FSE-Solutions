d667474521b91bc8495b697393372f6c3ca88db0
percyjack123 <23052643@kiit.ac.in>
2026-07-01 22:14:47 +0530

    Initial commit - Cognizant DN5 Solutions

---
+++ Module1-DesignPatterns/SingletonPatternExample/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+
+        logger2.log("User Logged In");
+
+        System.out.println(
+                "Same Object ? " +
+                        (logger1 == logger2)
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Cognizant DN 5.0 Solutions
+
+This repository contains my solutions for the Cognizant DN 5.0 Java FSE Deep Skilling Program.
+
+## Modules
+- Module 1 - Design Patterns and Principles
+- Module 2 - Data Structures and Algorithms
+- Module 3 - PL/SQL Programming
+- Module 4 - JUnit, Mockito and SLF4J
+- Module 5 - Spring Core and Maven
+- Module 6 - Spring Data JPA and Hibernate
