b2656635c18ad83087c88b190c9e033004d66091
percyjack123 <23052643@kiit.ac.in>
2026-07-01 22:28:55 +0530

    Implemented Exercise 7 - Financial Forecasting

---
+++ Module2-DSA/Exercise7-FinancialForecast/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double futureValue = FinancialForecast.calculateFutureValue(
+                currentValue,
+                growthRate,
+                years
+        );
+
+        System.out.printf("Future Value after %d years = %.2f%n", years, futureValue);
+
+    }
+
+}
